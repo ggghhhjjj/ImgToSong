494c92f154c37e052ad9cb28b6e49ba5d91c1b5e
George Shumakov <gshumakov@ariba.com>
2015-05-05 23:57:42 +0300

    Finished windows application

--- src/main/java/george/shumakov/imgtosong/loader/RsrcURLStreamHandlerFactory.java
+++ src/main/java/george/shumakov/imgtosong/loader/RsrcURLStreamHandlerFactory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package george.shumakov.imgtosong.loder;
+package george.shumakov.imgtosong.loader;
 
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
